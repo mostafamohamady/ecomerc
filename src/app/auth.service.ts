8539b6d08a90a67a6d4275db0cc996db724f4c03
Mostafa <a.sakr@cch-kuwait.com>
2024-03-05 18:29:53 +0200

    sssssss

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,39 +1,32 @@
-
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
-import { Observable } from 'rxjs';
-Observable
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  userData = new BehaviorSubject(null);
+  constructor(private _HttpClient: HttpClient) {}
 
-
-  userData=null;
-  constructor(private _HttpClient:HttpClient) { }
-
-  decodedUserData(){
-    
-    let encodedToken =JSON.stringify(localStorage.getItem('userToken'));
-    let decodedToken :any=jwtDecode(encodedToken);
+  decodedUserData() {
+    let encodedToken = JSON.stringify(localStorage.getItem('userToken'));
+    let decodedToken: any = jwtDecode(encodedToken);
     console.log(decodedToken);
-    this.userData=decodedToken; 
-
+    this.userData.next(decodedToken);
   }
 
-  register(userData:object):Observable<any>
-  {
-    return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signup',userData)
-
+  register(userData: object): Observable<any> {
+    return this._HttpClient.post(
+      'https://ecommerce.routemisr.com/api/v1/auth/signup',
+      userData
+    );
   }
-  login(userData:object):Observable<any> {
-
-    return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signin',userData)
-
+  login(userData: object): Observable<any> {
+    return this._HttpClient.post(
+      'https://ecommerce.routemisr.com/api/v1/auth/signin',
+      userData
+    );
   }
-
-
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  isLoged: boolean = false;
 
+  constructor(private _AuthService: AuthService) {
+    _AuthService.userData.subscribe({
+      next: () => {
+        if (_AuthService.userData.getValue() !== null) {
+          this.isLoged = true;
+        } else {
+          this.isLoged=false
+        }
+      },
+    });
+  }
 }
