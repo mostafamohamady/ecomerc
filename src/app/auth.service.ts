358355394ace0dbe86332fc67d65c094ecb33e63
Mostafa <a.sakr@cch-kuwait.com>
2024-03-05 10:37:24 +0200

    sssssss

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
 Observable
 
@@ -10,7 +11,19 @@ Observable
 })
 export class AuthService {
 
+
+  userData=null;
   constructor(private _HttpClient:HttpClient) { }
+
+  decodedUserData(){
+    
+    let encodedToken =JSON.stringify(localStorage.getItem('userToken'));
+    let decodedToken :any=jwtDecode(encodedToken);
+    console.log(decodedToken);
+    this.userData=decodedToken; 
+
+  }
+
   register(userData:object):Observable<any>
   {
     return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signup',userData)
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,14 +13,12 @@ export class LoginComponent {
 constructor (private _AuthService:AuthService , private _Router:Router){
   
 }
-isLoading:boolean=false;
+  isLoading:boolean=false;
   apiError:string ='';
-
 
 loginForm:FormGroup=new FormGroup({
 email:new FormControl(null, [Validators.required,Validators.email]),
 password:new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)])
-
 
 })
 
@@ -32,7 +30,9 @@ handelLogin(loginForm:FormGroup){
     this._AuthService.login(loginForm.value).subscribe({
       next:(response)=>{
         if (response.message === 'success'){
-          
+        localStorage.setItem('userToken',response.token);
+        this._AuthService.decodedUserData();
+        
          this.isLoading=false;
           this._Router.navigate(['/home'])
         }
