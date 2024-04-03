6994ab9a15f1d7f78f65d16cf869aa319ee56500
Mostafa <a.sakr@cch-kuwait.com>
2024-04-03 14:22:55 +0200

    update

---
+++ src/app/products.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private _HttpClient: HttpClient) {
+    
+  }
+  getProduct(): Observable<any> {
+    return this._HttpClient.get(
+      'https://ecommerce.routemisr.com/api/v1/products'
+    );
+  }
+}
