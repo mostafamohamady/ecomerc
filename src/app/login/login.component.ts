2d28947f60c5df2316f8fd313c3bbd137e2b12f3
Mostafa <a.sakr@cch-kuwait.com>
2024-04-03 14:36:39 +0200

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,59 +1,45 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormControl,Validator, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private _AuthService: AuthService, private _Router: Router) {}
+  isLoading: boolean = false;
+  apiError: string = '';
 
-constructor (private _AuthService:AuthService , private _Router:Router){
-  
-}
-  isLoading:boolean=false;
-  apiError:string ='';
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/),
+    ]),
+  });
 
-loginForm:FormGroup=new FormGroup({
-email:new FormControl(null, [Validators.required,Validators.email]),
-password:new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{5,10}$/)])
+  handelLogin(loginForm: FormGroup) {
+    this.isLoading = true;
 
-})
-
-handelLogin(loginForm:FormGroup){
-
-  this.isLoading=true;
-
-  if(loginForm.valid){
-    this._AuthService.login(loginForm.value).subscribe({
-      next:(response)=>{
-        if (response.message === 'success'){
-        localStorage.setItem('userToken',response.token);
-        this._AuthService.decodedUserData();
-        
-         this.isLoading=false;
-          this._Router.navigate(['/home'])
-        }
-      },
-      error:(err)=>{
-        this.isLoading=false;
-        this.apiError=err.error.message;
-        console.log(err.error.message);
-        
-      
-      }
-    })
-
+    if (loginForm.valid) {
+      this._AuthService.login(loginForm.value).subscribe({
+        next: (response) => {
+          if (response.message === 'success') {
+            localStorage.setItem('userToken', response.token);
+            this._AuthService.decodedUserData();
+            this.isLoading = false;
+            this._Router.navigate(['/home']);
+          }
+        },
+        error: (err) => {
+          this.isLoading = false;
+          this.apiError = err.error.message;
+          console.log(err.error.message);
+        },
+      });
+    }
   }
-
 }
-
-
-
-
-}
-
-
-
