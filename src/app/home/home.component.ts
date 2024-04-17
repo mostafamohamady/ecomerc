8de47c1c8887f8193ac913db5a3b51aa812c8935
Mostafa <a.sakr@cch-kuwait.com>
2024-04-17 10:12:05 +0200

    sssssss

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: any[] = [];
+  constructor(private _ProductsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this._ProductsService.getProduct().subscribe({
+      next: (response) => (this.products = response.data),
+    });
+
+
+  }
+  
+
 
 }
