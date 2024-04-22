31590b1b4a7d21fcaa45e1fe30472862603bf68d
Mostafa <a.sakr@cch-kuwait.com>
2024-04-22 13:47:30 +0200

    update22-4-2024

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: any[] = [];
+  constructor(private _ProductsService: ProductsService) {}
 
+  ngOnInit(): void {
+    this._ProductsService.getCategories().subscribe({
+      next: (response) => {
+        this.categories = response.data;
+      },
+    });
+    console.log(this.categories);
+  }
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 7
+      }
+    },
+    nav: false,
+    autoplay:true
+  }
+  
 }
