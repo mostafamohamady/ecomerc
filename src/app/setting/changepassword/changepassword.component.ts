1092f02016b96a84c88356df99dd12c70435b109
Mostafa <a.sakr@cch-kuwait.com>
2024-05-23 17:10:56 +0300

    13-5-2024

---
+++ src/app/setting/changepassword/changepassword.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-changepassword',
+
+
+  templateUrl: './changepassword.component.html',
+  styleUrl: './changepassword.component.scss'
+})
+export class ChangepasswordComponent {
+
+}
---
+++ src/app/setting/resetpassword/resetpassword.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resetpassword',
+
+  templateUrl: './resetpassword.component.html',
+  styleUrl: './resetpassword.component.scss'
+})
+export class ResetpasswordComponent {
+
+}
